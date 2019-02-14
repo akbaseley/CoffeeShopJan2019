1010efd2ffb0274575acca5b62be4d1b93fc60f9
Anna B <akbaseley@gmail.com>
2019-02-14 09:46:30 -0500

    Jan 2019 C# Coffee Shop App

---
+++ CoffeeShopLabJan2019/Controllers/HomeController.cs
@@ -0,0 +1,52 @@
+﻿using CoffeeShopLabJan2019.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace CoffeeShopLabJan2019.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+
+        public ActionResult RegisterUser()
+        {
+            return View();
+        }
+
+        public ActionResult UserDetails(User newUser)
+        {
+            if (ModelState.IsValid)
+            {
+                ViewBag.CurrentUser = newUser;
+                return View();
+            }
+            else
+            {
+                ViewBag.ErrorMessage = "Registration failed. Try again.";
+                return View("Error");
+            }
+
+        }
+
+    }
+}
---
+++ CoffeeShopLabJan2019/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="CoffeeShopLabJan2019.MvcApplication" Language="C#" %>
